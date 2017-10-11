8b4debd870e88d5818fa48de4186f128f5ddbd58
sunhangye <sunhangye@chehejia.com>
2017-10-11 11:48:38 +0800

    sunhangye
    完成推荐页面

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="recommend" ref="recommend" >
-    <scroll class="recommend-content" :data="discList">
+    <scroll ref="scroll" class="recommend-content" :data="discList">
       <div>
         <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
           <slider>
             <div v-for="item in recommends">
               <a :href="item.linkUrl">
-                <img :src="item.picUrl" alt="">
+                <img @load="loadImage" :src="item.picUrl" alt="">
               </a>
             </div>
           </slider>
@@ -16,7 +16,7 @@
           <ul>
             <li v-if="discList.length" v-for="(item, index) in discList" class="item">
               <div class="icon">
-                <img width="60" height="60" :src="item.imgurl" alt="">
+                <img width="60" height="60" v-lazy="item.imgurl" alt="">
               </div>
               <div class="text">
                 <h2 class="name" v-html="item.creator.name"></h2>
@@ -26,7 +26,9 @@
           </ul>
         </div>
       </div>
-
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
+      </div>
     </scroll>
 
 
@@ -38,6 +40,7 @@
   import {ERROR_OK} from 'api/config'
   import Slider from 'base/slider/slider'
   import Scroll from 'base/scroll/scroll'
+  import Loading from 'base/loading/loading'
 
   export default {
     data(){
@@ -49,15 +52,12 @@
     created() {
       this._getRecommend()
       this._getDiscList()
-
     },
     methods: {
       _getRecommend() {
         getRecommend().then((res) => {
           if(res.code === ERROR_OK){
-
             this.recommends = res.data.slider
-
           }
         })
       },
@@ -67,11 +67,19 @@
             this.discList = res.data.list
           }
         })
+      },
+      loadImage(){
+        // 轮播图是动态生成 列表会少计算这一高度 所以等图片加载完刷新列表
+        if (!this.checkedLoad) {
+          this.$refs.scroll.refresh()
+          this.checkedLoad = true
+        }
       }
     },
     components: {
       Slider,
-      Scroll
+      Scroll,
+      Loading
     }
 
   }
@@ -117,5 +125,13 @@
               margin-bottom 10px
             .disc
               color $color-text-d
-
+    .slider-wrapper
+      position: relative
+      width: 100%
+      overflow: hidden
+    .loading-container
+      position: absolute
+      width: 100%
+      top: 50%
+      transform: translateY(-50%)
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 
 import Vue from 'vue'
 import App from './App'
- import router from './router'
+import router from './router'
 import VueRouter from 'vue-router'
+import VueLazyload from 'vue-lazyload'
 import fastclick from 'fastclick'
-
 
 import 'common/stylus/index.styl'
 
 Vue.config.productionTip = false
 
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
 fastclick.attach(document.body)
 
 
